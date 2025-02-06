09ce3313242a3c6a8dde3cafab3f4dd36d0ee0d7
alex <estudosdev2025@outlook.com>
2025-02-06 18:19:00 -0300

    wworkflows

--- obj/Debug/net8.0/AppCICD.AssemblyInfo.cs
+++ obj/Debug/net8.0/AppCICD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppCICD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac57e9adb4db87079b8f716eaef3e67bdbfae95c")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppCICD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppCICD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
